09c3ae29340f3d7e7cb575d4d6a766ef34d28455
kyorohiro <kyorohiro@gmail.com>
2017-02-12 06:30:37 +0900

    ..

--- web/main.dart
+++ web/main.dart
@@ -5,7 +5,7 @@ import 'dart:async';
 import 'imageutil.dart';
 
 
- main() async {
+main() async {
   Nonno nonno = new Nonno("assets/ic.jpg");
   html.document.body.append(nonno.element);
   await nonno.init();
@@ -22,12 +22,12 @@ class NTexture {
 
     tex.imageElement = new html.ImageElement();
     Completer comp = new Completer();
-    tex.imageElement.onLoad.listen((e)async {
+    tex.imageElement.onLoad.listen((e) async {
       print("found ${tex.imageElement.width} ${tex.imageElement.height}");
-      tex.imageElement = await ImageUtil.resizeImage(tex.imageElement,nextWidth: 512);
+      tex.imageElement = await ImageUtil.resizeImage(tex.imageElement, nextWidth: 512);
       comp.complete(tex);
     });
-    tex.imageElement.onError.listen((e){
+    tex.imageElement.onError.listen((e) {
       print("not found");
       comp.completeError(e);
     });
@@ -35,21 +35,20 @@ class NTexture {
     return comp.future;
   }
 
-  create(gl.RenderingContext context){
+  create(gl.RenderingContext context) {
     this.texture = context.createTexture();
     context.bindTexture(gl.TEXTURE_2D, texture);
-    context.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,gl.UNSIGNED_BYTE, this.imageElement);
+    context.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this.imageElement);
     context.generateMipmap(gl.TEXTURE_2D);
-
   }
-}
-class Nonno {
-  final int width;
-  final int height;
-  final int cellSize;
-  html.CanvasElement _canvas;
-  final String texturePath;
 
+  int get widht => imageElement.clientWidth;
+
+  int get height => imageElement.clientHeight;
+
+}
+
+class NProgram {
   // calc position
   String vertextShaderSrc = const [
     "attribute vec3 vertexPosition;",
@@ -76,17 +75,20 @@ class Nonno {
     "}",
   ].join("\r\n");
 
-  Nonno(this.texturePath, {this.width: 600, this.height: 400, this.cellSize: 20}) {
-    _canvas = new html.CanvasElement(width: this.width, height: this.height);
-  }
+  int _vertexPositionLocation;
+  int _colorLocation;
+  int _texCoordLocation;
 
-  init() async {
-    double ratioHW = width/height;
-    gl.RenderingContext context = _canvas.getContext3d();
-    context.viewport(0, 0, this.width, this.height);
+  int get vertexPositionLocation => _vertexPositionLocation;
 
-    //
-    // compile shader
+  int get colorLocation => _colorLocation;
+
+  int get texCoordLocation => _texCoordLocation;
+
+  gl.Program get program => _program;
+  gl.Program _program;
+
+  gl.Program compile(gl.RenderingContext context, {bool doUse: true}) {
     gl.Shader vertexS = context.createShader(gl.VERTEX_SHADER);
     gl.Shader fragmS = context.createShader(gl.FRAGMENT_SHADER);
     context.shaderSource(vertexS, vertextShaderSrc);
@@ -103,21 +105,48 @@ class Nonno {
       context.getShaderInfoLog(fragmS)
       ].join("\r\n"));
     }
+
     //
     // link program
-    gl.Program program = context.createProgram();
+    _program = context.createProgram();
     context.attachShader(program, vertexS);
     context.attachShader(program, fragmS);
     context.linkProgram(program);
+
     if (false == context.getProgramParameter(program, gl.LINK_STATUS)) {
       throw new Exception(["failed to link program: ", //
       context.getProgramInfoLog(program)
       ].join("\r\n"));
     }
-    context.useProgram(program);
-    var vertexPositionLocation = context.getAttribLocation(program, "vertexPosition");
-    var colorLocation = context.getAttribLocation(program, "color");
-    var texCoordLocation       = context.getAttribLocation(program, "texCoord");
+    _vertexPositionLocation = context.getAttribLocation(program, "vertexPosition");
+    _colorLocation = context.getAttribLocation(program, "color");
+    _texCoordLocation = context.getAttribLocation(program, "texCoord");
+
+    context.useProgram(_program);
+    return program;
+  }
+}
+
+class Nonno {
+  final int width;
+  final int height;
+  final int cellSize;
+  html.CanvasElement _canvas;
+  final String texturePath;
+
+  Nonno(this.texturePath, {this.width: 600, this.height: 400, this.cellSize: 20}) {
+    _canvas = new html.CanvasElement(width: this.width, height: this.height);
+  }
+
+  gl.RenderingContext context;
+  
+  init() async {
+    double ratioHW = width / height;
+    context = _canvas.getContext3d();
+    context.viewport(0, 0, this.width, this.height);
+
+    NProgram nprogram = new NProgram();
+    nprogram.compile(context);
 
     //
     NTexture nTexture = await NTexture.newTexture(texturePath);
@@ -128,31 +157,31 @@ class Nonno {
     final vSize = 3;
     final cSize = 4;
     final tSize = 2;
-    final strideSize = (9)*Float32List.BYTES_PER_ELEMENT;
-    final colorOffset = (3)*Float32List.BYTES_PER_ELEMENT;
-    final texOffset = (7)*Float32List.BYTES_PER_ELEMENT;
+    final strideSize = (9) * Float32List.BYTES_PER_ELEMENT;
+    final colorOffset = (3) * Float32List.BYTES_PER_ELEMENT;
+    final texOffset = (7) * Float32List.BYTES_PER_ELEMENT;
     gl.Buffer vertexBuffer = context.createBuffer();
     gl.Buffer indexBuffer = context.createBuffer();
 
     context.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    context.enableVertexAttribArray(vertexPositionLocation);
-    context.enableVertexAttribArray(colorLocation);
-    context.enableVertexAttribArray(texCoordLocation);
-    context.vertexAttribPointer(vertexPositionLocation, vSize, gl.FLOAT, false, strideSize, 0);
-    context.vertexAttribPointer(colorLocation, cSize, gl.FLOAT, false, strideSize, colorOffset );
-    context.vertexAttribPointer(texCoordLocation, tSize, gl.FLOAT, false, strideSize, texOffset );
+    context.enableVertexAttribArray(nprogram.vertexPositionLocation);
+    context.enableVertexAttribArray(nprogram.colorLocation);
+    context.enableVertexAttribArray(nprogram.texCoordLocation);
+    context.vertexAttribPointer(nprogram.vertexPositionLocation, vSize, gl.FLOAT, false, strideSize, 0);
+    context.vertexAttribPointer(nprogram.colorLocation, cSize, gl.FLOAT, false, strideSize, colorOffset);
+    context.vertexAttribPointer(nprogram.texCoordLocation, tSize, gl.FLOAT, false, strideSize, texOffset);
 
     var vertices = new Float32List.fromList(<double>[
-      -0.5, 0.5*ratioHW, 0.0,  /**/1.0, 0.0, 0.0, 1.0, /**/ 0.0,0.0,
-      -0.5, -0.5*ratioHW, 0.0, /**/0.0, 1.0, 0.0, 1.0, /**/ 0.0,1.0,
-      0.5, 0.5*ratioHW, 0.0,   /**/0.0, 0.0, 1.0, 1.0, /**/ 1.0,0.0,
-      0.5, -0.5*ratioHW, 0.0,  /**/0.0, 0.0, 0.0, 1.0, /**/ 1.0,1.0,
+      -0.5, 0.5 * ratioHW, 0.0, /**/1.0, 0.0, 0.0, 1.0, /**/ 0.0, 0.0,
+      -0.5, -0.5 * ratioHW, 0.0, /**/0.0, 1.0, 0.0, 1.0, /**/ 0.0, 1.0,
+      0.5, 0.5 * ratioHW, 0.0, /**/0.0, 0.0, 1.0, 1.0, /**/ 1.0, 0.0,
+      0.5, -0.5 * ratioHW, 0.0, /**/0.0, 0.0, 0.0, 1.0, /**/ 1.0, 1.0,
     ]);
 
     var indexs = new Uint16List.fromList(<int>[
-      0,1,2,//
-      2,3,1
-      ]);
+      0, 1, 2, //
+      2, 3, 1
+    ]);
     //
     //
 
@@ -161,7 +190,7 @@ class Nonno {
     context.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     context.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexs, gl.STATIC_DRAW);
     // context.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-    context.clearColor(0.0,0.3,0.3,0.5);
+    context.clearColor(0.0, 0.3, 0.3, 0.5);
     context.clear(gl.COLOR_BUFFER_BIT);
     context.drawElements(gl.TRIANGLES, indexs.length, gl.UNSIGNED_SHORT, 0);
     context.flush();
